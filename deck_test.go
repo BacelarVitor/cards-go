f10d88b11c7a70a24ac70f92587d69c0711dce07
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-03-24 20:18:27 -0300

    add some card position tests

--- deck_test.go
+++ deck_test.go
@@ -9,3 +9,24 @@ func TestNewDeck(t *testing.T) {
 	}
 }
 
+func TestFirstCardIsAceOfSpades(t *testing.T) {
+	d := newDeck()
+
+	e := "Ace of Spades"
+	testCardPosition(t, d, 0, e)
+}
+
+
+func TestLastCardIsAceOfSpades(t *testing.T) {
+	d := newDeck()
+	
+	i := len(d) -1
+	e := "King of Diamonds"
+	testCardPosition(t, d, i, e)
+}
+
+func testCardPosition(t *testing.T, d deck, i int, e string) {
+	if d[i] !=  e {
+		t.Errorf("Expected first card of %v, but got %v", e, d[i])
+	}
+}
